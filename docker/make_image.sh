286bb47dd766a6e0504e83c7afe657c43702a078
Jon Hasselgren <jhasselgren@nvidia.com>
2023-11-20 14:40:29 +0100

    Removed deprecated nvidia-docker from dockerfile

--- docker/make_image.sh
+++ docker/make_image.sh
@@ -13,6 +13,6 @@ docker build -f Dockerfile -t $1 .
 # This is a workaround to avoid having to reconfigure docker to build images using nvidia runtime: https://github.com/NVIDIA/nvidia-docker/wiki/Advanced-topics#default-runtime.
 # Changing docker settings require root privileges.
 rm ./tmp.cid
-nvidia-docker run --cidfile tmp.cid -it $1 pip install --global-option="--no-networks" git+https://github.com/NVlabs/tiny-cuda-nn/#subdirectory=bindings/torch
-nvidia-docker commit $(< tmp.cid) $1
+docker run --gpus device=0 --cidfile tmp.cid -it $1 pip install --global-option="--no-networks" git+https://github.com/NVlabs/tiny-cuda-nn/#subdirectory=bindings/torch
+docker commit $(< tmp.cid) $1
 rm ./tmp.cid
